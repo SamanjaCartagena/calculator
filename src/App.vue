44646cbf2ae129094a7ae1f257feeaf1f983969e
SamanjaCartagena <cart.samanja09@gmail.com>
2022-03-20 12:27:24 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CalculatorN />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorN from './components/CalculatorN.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculatorN
   }
 }
 </script>
---
+++ src/components/CalculatorN.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="calculator">
+    <div class="display">0.052342</div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">X</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+        <div class="btn">2</div>
+        <div class="btn">3</div>
+        <div class="btn operator">+</div>
+        <div class="btn zero">0</div>
+        <div class="btn">.</div>
+        <div class="btn operator">=</div>
+
+</div>
+</template>
+
+<script>
+
+export default {
+  
+  
+}
+</script>
+
+<style scoped>
+.calculator{
+    display:grid;
+    font-size: 30px;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows:minmax(50px, auto);
+}
+.display{
+    grid-column:1/5;
+    background-color: red;;
+}
+.zero{
+    grid-column: 1 / 3;
+}
+.btn{
+    background-color: #eee;
+    border:1px solid #333;
+}
+.operator{
+    background-color: orange;
+    color:white; 
+}
+</style>
